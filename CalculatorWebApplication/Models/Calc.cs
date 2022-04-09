4d36ba4703b36a1929f3aad2f22eea170125d04a
Raman <Raman@DESKTOP-UUFUMDH>
2022-04-08 23:17:54 -0400

    adding some changes

---
+++ CalculatorWebApplication/Models/Calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorWebApplication.Models
+{
+    public class Calc
+    {
+        public int Input1 { get; set; }
+        public int Input2 { get; set; }
+        public int Result { get; set; }
+    } 
+}
