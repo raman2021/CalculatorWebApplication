6d22f9a9de33b8a310b38abae89cb5a6ed057eea
Raman <Raman@DESKTOP-UUFUMDH>
2022-04-08 20:31:17 -0400

    creating 2 new project one is library second is for testing

---
+++ CalculatorTestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
